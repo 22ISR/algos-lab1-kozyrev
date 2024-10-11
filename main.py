7aeeeedc0959506d2e2f1727b4ce07676ca579fe
Ev <gggecktor@gmail.com>
2024-10-11 13:29:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ hint: цикл, если и "%"
 hint: https://ru.wikihow.com/%D0%B2%D1%8B%D1%81%D1%87%D0%B8%D1%82%D1%8B%D0%B2%D0%B0%D1%82%D1%8C-%D0%B2%D0%B8%D1%81%D0%BE%D0%BA%D0%BE%D1%81%D0%BD%D1%8B%D0%B5-%D0%B3%D0%BE%D0%B4%D1%8B
 """
 # x = int(input("Введите год: "))
-# if x % 4 ==0 and x % 100 != 0):
+# if (x % 4 ==0 and x % 100 != 0) and (x % 400 == 0):
 #   print (x, " - Год високосный")
 # else:
 #   print (x, " - Год невисокосный")
